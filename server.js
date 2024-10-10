c0fca71fd3d996bf13674fd4532ef30757339e55
tolnabert <tolnabert@gmail.com>
2024-10-10 19:48:59 +0200

    impl get and post request both FE and BE

--- server.js
+++ server.js
@@ -10,18 +10,42 @@ const PORT = 3000;
 const readTodos = () => {
   try {
     const data = fs.readFileSync('todos.json');
-    console.log(JSON.parse(data));
-
+    console.log("readtodos data: " + data);
+    
     return JSON.parse(data);
   } catch (error) {
-    console.error(error);
+
+    console.error("catch error " + error);
     return [];
   }
 };
 
-const writeFile = (todos) => {
+const writeTodos = (todos) => {
   fs.writeFileSync('todos.json', JSON.stringify(todos), null, 2);
 };
+
+const getHighestId = (todos) => {
+  if (todos.length === 0) return 1;
+  return Math.max(...todos.map((todo) => todo.id));
+};
+
+app.get('/todos', (req, res) => {
+  const todos = readTodos();
+  res.json(todos);
+});
+
+app.post('/todos', (req, res) => {
+  const newTodo = req.body;
+  const todos = readTodos();
+
+  const highestId = getHighestId(todos);
+
+  newTodo.id = highestId;
+
+  const updatedTodos = [...todos, newTodo];
+  writeTodos(updatedTodos);
+  res.status(201).json({ message: 'added todo', newTodo: newTodo });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port: ${PORT}...`);
