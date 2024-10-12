d2316525b519809f3f7603dcaf8c8a3104d94003
tintapolipka <109940436+tintapolipka@users.noreply.github.com>
2024-10-12 12:24:42 +0200

    listTodos is ready

--- client.js
+++ client.js
@@ -1,30 +1,51 @@
-fetch('http://localhost:3000/todos')
-  .then((response) => response.json())
-  .then((data) => {
-    // console.log(data);
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+async function listTodos() {
+  async function fetchTodos() {
+    try {
+      const response = await fetch("http://localhost:3000/todos");
+      return await response.json();
+    } catch (error) {
+      console.error(error);
+      return error.message;
+    }
+  }
+  return await fetchTodos();
+}
+//TEST
+(async () => {
+  const todos = await listTodos();
+  console.warn("VAN-E?", todos);
+})();
 
-fetch('http://localhost:3000/todos', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({
-    title: 'newtodo_title',
-    description: 'newtodo_description',
-  }),
-});
+// function listTodos(){
+// fetch('http://localhost:3000/todos')
+//   .then((response) => response.json())
+//   .then((data) => {
+//     console.log(data);
+//     return data;
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//   });
+// }
 
-fetch('http://localhost:3000/todos', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({
-    title: 'newtodo_title-2',
-    description: 'newtodo_description-2',
-  }),
-});
+// fetch('http://localhost:3000/todos', {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+//   body: JSON.stringify({
+//     title: 'newtodo_title',
+//     description: 'newtodo_description',
+//   }),
+// });
+
+// fetch('http://localhost:3000/todos', {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+//   body: JSON.stringify({
+//     title: 'newtodo_title-2',
+//     description: 'newtodo_description-2',
+//   }),
+// });
